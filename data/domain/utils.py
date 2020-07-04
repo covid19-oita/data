c273da04bcf21670a4a1d24d28e2d221de21e820
Shoma Okamoto <32533860+shmokmt@users.noreply.github.com>
2020-07-04 14:58:14 +0900

    add method.

--- data/domain/utils.py
+++ data/domain/utils.py
@@ -1,6 +1,8 @@
 import csv
+from datetime import datetime
 import io
 from typing import Dict, List
+from dateutil.rrule import DAILY, rrule
 import requests
 
 
@@ -16,3 +18,22 @@ def read_csv(url: str) -> List[Dict[str, str]]:
 
 def join_fintype():
     pass
+
+
+def date_range(start_date, end_date):
+    rule = rrule(freq=DAILY, dtstart=start_date, until=end_date)
+    for date in list(rule):
+        yield date
+
+
+def format_date(date: str):
+    """
+    入力された日付文字列(m月d日)に現在の年情報を追加します。
+    Args:
+    date: str: 日付文字列(m月d日)
+    Returns:
+    str: 日付文字列(Y年m月d日)
+    NOTE: 2021年にも対応できるように修正する必要があります。
+    """
+    year = datetime.now().year
+    return f"{year}年{date}"
