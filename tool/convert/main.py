a467ca338b7b43fd3e9da4c91739ae46dd885f02
Shoma Okamoto <32533860+shmokmt@users.noreply.github.com>
2020-07-04 14:51:56 +0900

    delete rss lines.

--- tool/convert/main.py
+++ tool/convert/main.py
@@ -2,7 +2,6 @@ import json
 import os
 import modules.PatientDataHandler as pdhandler
 import modules.FinancialDataHandler as fdhandler
-import modules.RSSHandler as rss
 
 PATIENTS_CSVFILE = "440001oitacovid19patients.csv"
 DATA_SUMMARY_CSVFILE = "440001oitacovid19datasummary.csv"
@@ -16,10 +15,10 @@ RSS_URL = "http://www.pref.oita.jp/rss/10/site-1000786.xml"
 
 def main():
     csvfile_dir = "/../../csv/"
-    patients_csvfile = os.path.dirname(
-        __file__) + csvfile_dir + PATIENTS_CSVFILE
-    data_summary_csvfile = os.path.dirname(
-        __file__) + csvfile_dir + DATA_SUMMARY_CSVFILE
+    patients_csvfile = os.path.dirname(__file__) + csvfile_dir + PATIENTS_CSVFILE
+    data_summary_csvfile = (
+        os.path.dirname(__file__) + csvfile_dir + DATA_SUMMARY_CSVFILE
+    )
     export_json_dir = os.path.dirname(__file__) + EXPORT_JSON_DIR
 
     pdh = pdhandler.PatientDataHandler(
@@ -36,25 +35,23 @@ def main():
 
     fdh = fdhandler.FinancialDataHandler(
         # TODO: あとで変数にする
-        loan_amount_data_csv=os.path.dirname(
-            __file__) + csvfile_dir + "440001oitacovid19finamount.csv",
-        loan_counts_data_csv=os.path.dirname(
-            __file__) + csvfile_dir + "440001oitacovid19finnumber.csv",
-        industry_master_data_csv=os.path.dirname(
-            __file__) + csvfile_dir + "440001oitacovid19fintype.csv",
-        subsidy_data_csv=os.path.dirname(
-            __file__) + csvfile_dir + "440001oitaemploymentsubsidy.csv"
+        loan_amount_data_csv=os.path.dirname(__file__)
+        + csvfile_dir
+        + "440001oitacovid19finamount.csv",
+        loan_counts_data_csv=os.path.dirname(__file__)
+        + csvfile_dir
+        + "440001oitacovid19finnumber.csv",
+        industry_master_data_csv=os.path.dirname(__file__)
+        + csvfile_dir
+        + "440001oitacovid19fintype.csv",
+        subsidy_data_csv=os.path.dirname(__file__)
+        + csvfile_dir
+        + "440001oitaemploymentsubsidy.csv",
     )
     financial_json = fdh.generate_data()
 
     with open(export_json_dir + "financial.json", 'w') as f:
         json.dump(financial_json, f, indent=2, ensure_ascii=False)
-
-    rss_handler = rss.RSSHandler()
-    news_json = rss_handler.generate_news(RSS_URL)
-
-    with open(export_json_dir + "news.json", 'w') as f:
-        json.dump(news_json, f, indent=2, ensure_ascii=False)
 
 
 if __name__ == "__main__":
