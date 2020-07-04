16c8ba4c8b9d597f2e731a0190fb83c1370038f0
Shoma Okamoto <32533860+shmokmt@users.noreply.github.com>
2020-07-04 14:59:04 +0900

    delete the file.

--- data/utils.py
+++ data/utils.py
@@ -1,9 +1,5 @@
 from datetime import timedelta
+from datetime import datetime
 from dateutil.relativedelta import relativedelta, SU
 from dateutil.rrule import rrule, DAILY
 
-
-def date_range(start_date, end_date):
-    rule = rrule(freq=DAILY, dtstart=start_date, until=end_date)
-    for date in list(rule):
-        yield date
