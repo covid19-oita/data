62fbf9300f6a09b2a3845b48cde7d17ce2902110
Daiki <nojiri1098@gmail.com>
2021-02-06 17:21:55 +0900

    Update TOTAL_SICKBEDS 330 => 365
    自動更新したい

--- tool/convert/modules/TestDataHandler.py
+++ tool/convert/modules/TestDataHandler.py
@@ -8,7 +8,7 @@ import DataHandler as handler
 
 PATIENTS_CSVFILE = "test_440001oitacovid19patients.csv"
 DATA_SUMMARY_CSVFILE = "test_440001oitacovid19datasummary.csv"
-TOTAL_SICKBEDS = 330
+TOTAL_SICKBEDS = 365
 
 
 class ConvertTest(unittest.TestCase):
